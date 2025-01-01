9f89ea786d7d5cecfa841683c3e0de0c6d6486c7
aya-darraji <darrajiaya@fstsbz.u-kairouan.tn>
2025-01-01 20:39:19 +0100

    fixbugAutoSve

--- app.js
+++ app.js
@@ -21,7 +21,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
 app.use('/cars', carsRouter);
 app.use('/users', usersRouter);
 app.use('/os', osRouter);
--- routes/users.js
+++ routes/users.js
@@ -6,7 +6,7 @@ const uploadFile = require('../middlewares/uploadFile');
 router.get('/getAllUsers',userController.getAllUsers );
 router.post('/addUserClient',userController.addUserClient );
 router.post('/updateUserWithImg',uploadFile.single("image_user"),userController.updateUserWithImg );
-router.delete('/deletedUser/:id',userController.deletedUser );
+router.delete('/deletedUser/:id',userController.deleteUser );
 router.put('updateUser/:id',userController.updateUser );
 router.put('updatepassword /:id',userController.updatepassword );
 router.get('getSortUsersByAge',userController.getSortUsersByAge );
